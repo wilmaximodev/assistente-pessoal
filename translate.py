49f890e8887eede9ad618a9cff326bea66859f25
Wilson Maximo <wilson-max@hotmail.com>
2025-02-03 22:46:35 -0300

    Funcionalidade de traduzir textos

---
+++ translate.py
@@ -0,0 +1,20 @@
+#translate.py
+import google.generativeai as genai
+import os
+
+GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
+genai.configure(api_key=GEMINI_API_KEY)
+
+model = genai.GenerativeModel(model_name='gemini-1.5-flash')
+
+with open("translated/documento_portugues.txt", "r", encoding="utf-8") as file:
+    texto_portugues = file.read()
+
+prompt = f"Por favor, traduza o seguinte texto para o inglês:\n{texto_portugues}"
+
+response = model.generate_content(prompt)
+
+with open("translated/documento_ingles.txt", "w", encoding="utf-8") as file:
+    file.write(response.text)
+print("Tradução concluída e salva em 'documento_ingles.txt'.")
+
